bcb6f49764062cec430651439f031058c402e78c
Jason Kridner <jdk@ti.com>
2020-09-10 13:24:33 -0400

    Need delay to avoid crash

--- sw/toggle_gp_led.sh
+++ sw/toggle_gp_led.sh
@@ -4,4 +4,4 @@ if (( $EUID != 0 )); then
   exit
 fi
 CHIP=`gpiodetect | grep "greybus_gpio" | awk '{print $1}'`
-VAL=0; for ((;;)); do VAL=$((VAL^1)); echo $VAL | gpioset ${CHIP} 6=${VAL}; done
+VAL=0; for ((;;)); do VAL=$((VAL^1)); echo $VAL | gpioset ${CHIP} 6=${VAL}; sleep 1; done
