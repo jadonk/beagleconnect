40c8d1b756e8d2699cf812c0a40685841f9c38f9
Jason Kridner <jkridner@beagleboard.org>
2020-10-11 21:37:55 +0000

    SW: buildroot: fixes

--- sw/buildroot/.br2-external.mk
+++ sw/buildroot/.br2-external.mk
@@ -2,9 +2,13 @@
 # Automatically generated file; DO NOT EDIT.
 #
 
-BR2_EXTERNAL ?=
+BR2_EXTERNAL ?= /home/debian/beagleconnect/sw/buildroot
 BR2_EXTERNAL_NAMES = 
 BR2_EXTERNAL_DIRS = 
 BR2_EXTERNAL_MKS = 
 
-# No br2-external tree defined.
+BR2_EXTERNAL_NAMES += beagleconnect_gateway_qemu
+BR2_EXTERNAL_DIRS += /home/debian/beagleconnect/sw/buildroot
+BR2_EXTERNAL_MKS += /home/debian/beagleconnect/sw/buildroot/external.mk
+export BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH = /home/debian/beagleconnect/sw/buildroot
+export BR2_EXTERNAL_beagleconnect_gateway_qemu_DESC = beagleconnect_gateway_qemu
