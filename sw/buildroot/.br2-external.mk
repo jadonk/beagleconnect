7a2391fd2eb214cfb32bddbbc32f9a91937a0423
Jason Kridner <jdk@ti.com>
2020-10-11 18:12:59 -0400

    SW: buildroot: save off .br2-external files

---
+++ sw/buildroot/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/jkridner/workspace/beagleconnect/sw/buildroot
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += beagleconnect_gateway_qemu
+BR2_EXTERNAL_DIRS += /home/jkridner/workspace/beagleconnect/sw/buildroot
+BR2_EXTERNAL_MKS += /home/jkridner/workspace/beagleconnect/sw/buildroot/external.mk
+export BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH = /home/jkridner/workspace/beagleconnect/sw/buildroot
+export BR2_EXTERNAL_beagleconnect_gateway_qemu_DESC = beagleconnect_gateway_qemu
