f199e657f23c6ca59138247acff9c0d6351cfa80
Jason Kridner <jdk@ti.com>
2020-10-14 23:44:31 -0400

    SW: buildroot: add gbridge

---
+++ sw/buildroot/package/gbridge/gbridge.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# gbridge
+#
+################################################################################
+
+GBRIDGE_VERSION = 0.0.2
+GBRIDGE_SITE = $(BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH)/../gbridge
+GBRIDGE_SITE_METHOD = local
+GBRIDGE_LICENSE = GPL-2.0
+GBRIDGE_LICENSE_FILES = LICENSE
+GBRIDGE_DEPENDENCIES = host-pkgconf
+
+GBRIDGE_AUTORECONF = YES
+GBRIDGE_AUTORECONF_OPTS = -vfi
+
+GBRIDGE_CONF_OPTS = \
+		   GBNETLINKDIR=$(BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH)/../greybus \
+		   --enable-uart \
+		   --enable-tcpip \
+		   --disable-gbsim \
+		   --enable-netlink \
+		   --disable-bluetooth
+
+$(eval $(autotools-package))
