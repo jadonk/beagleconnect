12144c7db0d388ea91e2ec53d19d4259b72d9be7
Jason Kridner <jdk@ti.com>
2020-10-15 20:58:07 -0400

    SW: buildroot: simply missed files!

---
+++ sw/buildroot/package/greybus-netlink-modules/greybus-netlink-driver.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# greybus-netlink-modules
+#
+################################################################################
+
+GREYBUS_NETLINK_MODULES_SITE = $(BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH)/../greybus
+GREYBUS_NETLINK_MODULES_SITE_METHOD = local
+GREYBUS_NETLINK_MODULES_LICENSE = GPL-2.0
+GREYBUS_NETLINK_MODULES_LICENSE_FILES = LICENSE
+
+GREYBUS_NETLINK_MODULES_MODULE_MAKE_OPTS = \
+    KERNELVER='$(LINUX_VERSION)' KERNELDIR='$(LINUX_DIR)' \
+    CC='$(TARGET_CC)' LD='$(TARGET_LD)'
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
