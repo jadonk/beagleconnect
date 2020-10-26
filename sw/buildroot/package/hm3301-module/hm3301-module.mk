5026c7996d908e1007bdb823fd1bf708cc6f32df
Jason Kridner <jdk@ti.com>
2020-10-25 21:50:29 -0400

    SW: buildroot: try to add HM3301 driver

---
+++ sw/buildroot/package/hm3301-module/hm3301-module.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# hm3301-module
+#
+################################################################################
+
+HM3301_MODULE_SITE = $(BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH)/../linux/hm3301
+HM3301_MODULE_SITE_METHOD = local
+HM3301_MODULE_LICENSE = GPL-2.0
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
