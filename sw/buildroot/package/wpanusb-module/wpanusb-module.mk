1ca1af85f6faec04c06baa439ebc1dd70c598725
Jason Kridner <jdk@ti.com>
2020-10-29 18:15:54 -0400

    SW: buildroot: add wpanusb

---
+++ sw/buildroot/package/wpanusb-module/wpanusb-module.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# wpanusb-module
+#
+################################################################################
+
+WPANUSB_MODULE_SITE = $(BR2_EXTERNAL_beagleconnect_gateway_qemu_PATH)/../linux/wpanusb
+WPANUSB_MODULE_SITE_METHOD = local
+WPANUSB_MODULE_LICENSE = GPL-2.0
+
+WPANUSB_MODULE_MAKE_OPTS = KDIR=$(LINUX_VERSION_PROBED)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
