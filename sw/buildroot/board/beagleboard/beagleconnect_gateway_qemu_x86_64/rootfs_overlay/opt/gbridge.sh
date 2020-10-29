3c92cbc50f0d417cae56067f11b19d5042362102
Jason Kridner <jdk@ti.com>
2020-10-29 18:57:34 -0400

    SW: buildroot: try to load modules

--- sw/buildroot/board/beagleboard/beagleconnect_gateway_qemu_x86_64/rootfs_overlay/opt/gbridge.sh
+++ sw/buildroot/board/beagleboard/beagleconnect_gateway_qemu_x86_64/rootfs_overlay/opt/gbridge.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -xv
-#ATMOD=`lsmod | grep atusb`
-#if [ "$ATMOD" == "" ]; then
-#  modprobe atusb
-#fi
-#sleep 1
-
 IFLP=`ifconfig | grep lowpan`
 ip link set wpan0 down
 iwpan phy phy0 set channel 0 26
@@ -21,20 +15,6 @@ killall -9 gbridge
 sleep 1
 
 ping6 -I lowpan0 2001:db8::1 -c 5
-sleep 1
-
-NLMOD=`lsmod | grep gb_netlink`
-if [ "$NLMOD" == "" ]; then
-#  modprobe greybus
-  modprobe gb-netlink
-#  modprobe gb-bootrom
-  modprobe gb-gbphy
-#  modprobe gb-loopback
-#  modprobe gb-spilib
-#  modprobe gb-firmware
-#  modprobe gb-log
-#  modprobe gb-raw
-fi
 sleep 1
 
 #gbridge > /var/log/gbridge
