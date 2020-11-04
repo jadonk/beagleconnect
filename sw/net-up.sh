973925897a9bf007b3dd220d2ae3907c3c04f46c
Jason Kridner <jdk@ti.com>
2020-11-04 10:33:31 -0500

    SW: zephyr: rebase

--- sw/net-up.sh
+++ sw/net-up.sh
@@ -4,9 +4,9 @@ if (( $EUID != 0 )); then
   exit
 fi
 ip link set wpan0 down
-iwpan phy phy0 set channel 0 26
+iwpan phy `iwpan phy | grep -m1 wpan_phy | cut -d' ' -f2` set channel 0 1
 iwpan dev wpan0 set pan_id 0xabcd
 ip link add link wpan0 name lowpan0 type lowpan
 ip link set wpan0 up
 ip link set lowpan0 up
-ip -6 addr add 2001:db8::2/64 dev lowpan0
+ip -6 addr add 2001:db8::1/64 dev lowpan0
