797f47033a3fe1a0f59c47690672bb41b1d9d239
Jason Kridner <jdk@ti.com>
2020-11-05 09:54:42 -0500

    CI/README: zephyr: extra flags

--- sw/net-up.sh
+++ sw/net-up.sh
@@ -9,4 +9,4 @@ iwpan dev wpan0 set pan_id 0xabcd
 ip link add link wpan0 name lowpan0 type lowpan
 ip link set wpan0 up
 ip link set lowpan0 up
-ip -6 addr add 2001:db8::1/64 dev lowpan0
+ip -6 addr add 2001:db8::2/64 dev lowpan0
