584483ea993623bd31d24bbaaad6afdbe68f4959
Jason Kridner <jdk@ti.com>
2020-11-09 16:45:15 -0500

    SW: net-up.sh: move back to channel 1

--- sw/net-up.sh
+++ sw/net-up.sh
@@ -5,7 +5,7 @@ if (( $EUID != 0 )); then
 fi
 ip link set lowpan0 down
 ip link set wpan0 down
-iwpan phy `iwpan phy | grep -m1 wpan_phy | cut -d' ' -f2` set channel 0 0
+iwpan phy `iwpan phy | grep -m1 wpan_phy | cut -d' ' -f2` set channel 0 1
 iwpan dev wpan0 set pan_id 0xabcd
 ip link add link wpan0 name lowpan0 type lowpan
 ip link set wpan0 up
