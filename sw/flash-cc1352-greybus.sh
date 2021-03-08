1009b02db4d39b3f8f32d0933fdf8677b44e5933
Jason Kridner <jkridner@beagleboard.org>
2021-03-08 13:21:56 -0500

    WIP debuging greybus firmware and buildroot gateway

---
+++ sw/flash-cc1352-greybus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ve
+export PORT=${1:-/dev/ttyACM0}
+export PROJECT=${2:-build/greybus_mikrobus_beagleconnect}
+export SWDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export ZEPHYR_TOOLCHAIN_VARIANT=${ZEPHYR_TOOLCHAIN_VARIANT:-zephyr}
+export ZEPHYR_SDK_INSTALL_DIR=${ZEPHYR_SDK_INSTALL_DIR:-~/zephyr-sdk-0.11.4}
+export ZEPHYR_BASE=${ZEPHYR_BASE:-$SWDIR/zephyrproject/zephyr}
+
+echo Make sure to use the correct ttyACMx...
+lsusb | grep 2047:0aa5
+west flash -d $ZEPHYR_BASE/$PROJECT $PORT
+
+
