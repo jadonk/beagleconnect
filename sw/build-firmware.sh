92a90a9176505b6c93ddf85ed628d618a32fd027
Jason Kridner <jkridner@beagleboard.org>
2021-11-05 11:54:07 -0400

    CI: add tag to zip name

--- sw/build-firmware.sh
+++ sw/build-firmware.sh
@@ -5,6 +5,9 @@ source ./env.sh
 cd $SWDIR/usb_uart_bridge
 make clean
 make
+cd ..
+mkdir -p build/msp430
+cp $SWDIR/usb_uart_bridge/usb_uart_bridge* build/msp430/
 
 # CC1352
 cd $ZEPHYR_BASE
@@ -22,14 +25,15 @@ west build -p always -b beagleconnect_freedom $ZPRJ/greybus-for-zephyr-mikrobus/
 west build -p always -b beagleconnect_freedom $ZPRJ/wpanusb_bc -d $ZEPHYR_BASE/build/wpanusb_beagleconnect -- -DOVERLAY_CONFIG=overlay-subghz.conf -DBOARD_ROOT=$ZPRJ/wpanusb_bc -DCONFIG_NET_CONFIG_IEEE802154_RADIO_TX_POWER=14 -DCONFIG_IEEE802154_CC13XX_CC26XX_SUB_GHZ_DIV_SETUP_PA=1 -DCONFIG_IEEE802154_CC13XX_CC26XX_SUB_GHZ_CS_THRESHOLD=-20
 
 # make a FW release directory with the FW binaries and relevant debug info
-mkdir -p $SWDIR/bcf-zephyr-fw
-cp $SWDIR/cc2538-bsl.py  $SWDIR/bcf-zephyr-fw/
+mkdir -p $SWDIR/build/cc1352
+mkdir -p $SWDIR/build/msp430
+cp $SWDIR/cc2538-bsl.py  $SWDIR/build/
 
 copy_fwbin () {
 		if [ -f $ZEPHYR_BASE/build/$1/zephyr/zephyr.bin ] ; then
-			cp $ZEPHYR_BASE/build/$1/zephyr/zephyr.bin $SWDIR/bcf-zephyr-fw/$1.bin
-			cp $ZEPHYR_BASE//build/$1/zephyr/zephyr.dts $SWDIR/bcf-zephyr-fw/$1.dts
-			cp $ZEPHYR_BASE/build/$1/zephyr/.config $SWDIR/bcf-zephyr-fw/$1.config
+			cp $ZEPHYR_BASE/build/$1/zephyr/zephyr.bin $SWDIR/build/cc1352/$1.bin
+			cp $ZEPHYR_BASE/build/$1/zephyr/zephyr.dts $SWDIR/build/cc1352/$1.dts
+			cp $ZEPHYR_BASE/build/$1/zephyr/.config $SWDIR/build/cc1352/$1.config
 		fi
 }
 
